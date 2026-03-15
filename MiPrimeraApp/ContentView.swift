078c2c4d7b5d65f6a5f6519839bd9f049b119e3b
Eddye Carrillo <joseulloa10272@gmail.com>
2026-03-15 14:44:32 -0600

    Initial Commit

---
+++ MiPrimeraApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiPrimeraApp
+//
+//  Created by Eddye Carrillo Ulloa on 15/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiPrimeraApp/MiPrimeraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiPrimeraAppApp.swift
+//  MiPrimeraApp
+//
+//  Created by Eddye Carrillo Ulloa on 15/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MiPrimeraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
